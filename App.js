bbb8b7c7e7df091fb040ba41d15f9d2bfa5461e4
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-01-24 08:09:43 +0530

    navbar

--- App.js
+++ App.js
@@ -1,17 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * Header
+ * -Logo
+ * -Nav Items
+ * 
+ * Body
+ * -Search
+ * -Restaurant Container
+ *  -Restaurant Card
+ *     -image
+ *     -Name , star rating , cuisine,delivery time
+ * 
+ * Footer
+ * -Copyright
+ * -Links
+ * -Address
+ * -Contact
+ */
 
 
-const heading = <h1>HEllo ract</h1>
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+               <img className="logo" src="https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=360"></img>
+            </div>
+            <div className="navItems">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-
-const HeadingComponent = ()=> (
-    <div id="container" style={{textAlign:"center"}}>
-        <heading></heading>
-        <h1>Food App</h1>
+const RestaurantCard = () =>{
+    return (
+<div className="resCard">
+        <h3>Paragon</h3>
     </div>
-)
+    )
+    
+}
+
+const Body = ()=> {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+              <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = ()=> {
+    return (
+        <div className="App">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)

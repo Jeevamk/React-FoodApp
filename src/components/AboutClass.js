6187c5765456216fb4d2f3406ba14a0ba85cefce
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-02-01 16:14:37 +0530

    state in class component

--- src/components/AboutClass.js
+++ src/components/AboutClass.js
@@ -4,13 +4,26 @@ class UserClass extends React.Component{
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            count:0,
+            
+        }
     }
-    render(){
+    
+    
+    render() {
         const {name, location} =this.props;
+        const {count} =this.state;
+
         return(
-            <div>
-                <h1>{name}</h1>
-                <h2>{location}</h2>
+            <div className="count-countainer">
+                <h3>count:{count}</h3>
+                <button className="btn-count" onClick={()=>{
+                this.setState({count:this.state.count + 1})
+                }}>count increase</button>
+                <h1>Name:{name}</h1>
+                <h2>Place:{location}</h2>
             </div>
         )
     }
